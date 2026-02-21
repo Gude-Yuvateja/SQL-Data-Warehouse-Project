305b433930877a9b3f5b88594a0fccda8ba019c4
TEJA <g.yuvateja1870@gmail.com>
2026-02-21 21:36:30 +0530

    Add files via upload

---
+++ Scripts/SQLQuery 01 Database.sql
@@ -0,0 +1,19 @@
+-- CREATE DATABASE 'DATAWAREHOUSE'
+
+
+USE Master;
+
+-- Create the DataWarehouse' Database
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+
+-- Create Schemas
+
+CREATE SCHEMA Bronze;
+
+CREATE SCHEMA Sliver;
+
+CREATE SCHEMA Gold;
